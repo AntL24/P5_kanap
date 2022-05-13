399436291c3f73627ac25df8447557e317f57e6c
Antonin Lecuyer <antonin.l2405@gmail.com>
2022-05-13 19:08:35 +0200

    imageurl becomes id

--- front/js/script.js
+++ front/js/script.js
@@ -1,20 +1,37 @@
 /*on récupère la liste des products*/
 fetch("http://localhost:80/api/products")
-/*on retourne une url en particulier
-(le premier objet [0] de l'array)*/
 .then((res) => res.json())
-.then((data) => addProduct(data))
+.then((data) => {
+   return addProduct(data)
+})
     
 
+/*on retourne l'url associée à une id en particulier
+(le premier objet [0] de l'array)*/
+/*************************************************************
+* Fonction principale utilisant deux fonctions accessoires****
+*************************************************************/
 function addProduct(data){
-    const imageUrl = data[0].imageUrl
-    const anchor = document.createElement("a")
-    anchor.href = imageUrl
-    anchor.text = "canapé1"
+    const id = data[0]._id
     /* selectbyid aurait également pu fonctionner pour aller chercher #items.
     queryselector est plus généraliste, il ne fonctionne pas qu'avec les ID. */
+    const anchor = createAnchor(id)
+    appendchildren(anchor)
+}
+
+/************************
+*Fonctions accessoires**
+***********************/
+/* Création de l'anchor en fonction de l'id */
+function createAnchor(id) {
+    const anchor = document.createElement("a")
+    anchor.href = "./product.html?id=" + id
+    return anchor
+}
+/* On vérifie qu'il existe bien un item, puis on le lie à l'anchor */
+function appendchildren(anchor){
     const items = document.querySelector ("#items")
-    if (items !=null) {
-        items.appendChild(anchor)
+    if (items != null) {
+    items.appendChild(anchor)
     }
 }
